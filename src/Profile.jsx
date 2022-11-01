f7cdd717df33083ecabc0cbf444b0cbb72744dbe
Osaze <osareniyeosazee@gmail.com>
2022-11-02 00:47:59 +0100

    new push

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -12,21 +12,19 @@ const Profile = ()=>{
 <div className="one">
 <a href="http://training.zuri.team" id="btn_zuri" >Twiitter Link </a>
 </div>
-<div className="one">
+<div className="two">
 <a href="https://books.zuri.team" id="book" >Zuri Team</a>
 </div>
-<div className="one">
+<div className="three">
 <a href="http://books.zuri.team/python-for-beginners?ref_id=osazee" id="book_python">Zuri Books</a>
 </div>
-<div className="one">
+<div className="four">
 <a href="https://background.zuri.team" id="pitch" >Python Books</a>
 </div>
-<div className="one">
+<div className="five">
 <a href="http" id="book_design" >Design Books</a>
 </div>
-<div className="one">
 
-</div>
 </div>
 
 
