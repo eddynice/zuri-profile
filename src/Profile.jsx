7f94096303801fcf188635bb7fffa2cf56b952d1
Osaze <osareniyeosazee@gmail.com>
2022-11-02 22:53:55 +0100

    added responsive to it

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -2,7 +2,7 @@ import "./style.css"
 const Profile = ()=>{
 
 	return(
-		<div >
+		<div  className="main">
 			<div className="moveRight">
 			<svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.3261 8.50617C16.5296 8.3318 16.6313 8.24461 16.6686 8.14086C16.7013 8.0498 16.7013 7.9502 16.6686 7.85914C16.6313 7.75539 16.5296 7.6682 16.3261 7.49383L9.26719 1.44331C8.917 1.14315 8.74191 0.993063 8.59367 0.989386C8.46483 0.986191 8.34177 1.04279 8.26035 1.14269C8.16667 1.25764 8.16667 1.48825 8.16667 1.94948V5.52886C6.38777 5.84007 4.75966 6.74146 3.54976 8.09489C2.23069 9.57043 1.50103 11.48 1.5 13.4591V13.9691C2.37445 12.9157 3.46626 12.0638 4.70063 11.4716C5.78891 10.9495 6.96535 10.6403 8.16667 10.5588V14.0505C8.16667 14.5117 8.16667 14.7424 8.26035 14.8573C8.34177 14.9572 8.46483 15.0138 8.59367 15.0106C8.74191 15.0069 8.917 14.8569 9.26719 14.5567L16.3261 8.50617Z" stroke="#667085" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"></path></svg>
 			</div>
@@ -11,22 +11,22 @@ const Profile = ()=>{
 <div className="profile">
 <img src="/OS.jpg" alt="osazee" id="profile_img"/>
 <h3 id="twitter">EddyNice</h3>
-{/*osareniye osareniye id="slack" */}
+{/*osareniye osazee id="slack" */}
 </div>
 <div className="section">
 <div className="one">
-<a href="http://training.zuri.team" id="btn_zuri" >Twiitter Link </a>
+<a href="http://training.zuri.team" id="btn_zuri" >Twitter Link </a>
 </div>
-<div className="two">
+<div className="one">
 <a href="https://books.zuri.team" id="book" >Zuri Team</a>
 </div>
-<div className="three">
+<div className="one">
 <a href="http://books.zuri.team/python-for-beginners?ref_id=osazee" id="book_python">Zuri Books</a>
 </div>
-<div className="four">
+<div className="one">
 <a href="https://background.zuri.team" id="pitch" >Python Books</a>
 </div>
-<div className="five">
+<div className="one">
 <a href="http" id="book_design" >Design Books</a>
 </div>
 
